0d154c069d551785999c3338c614053ad1702ee8
james w hubert <jwhubert91@gmail.com>
2019-10-09 22:29:13 -0400

    First commit ever

---
+++ test.rb
@@ -0,0 +1,8 @@
+# test file for our first git homework project
+
+
+puts "Welcome to the first test file for your ruby homework!"
+puts "Enter your name: "
+name = gets.chomp
+puts "Hello " + name + " and welcome to Git!"
+puts "Now close this program because I have no more to say, please."
